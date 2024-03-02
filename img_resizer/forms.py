005972ca1a4b0b7d934ccd07491c8ddd063e7faf
idahmed <idahmedyassine@gmail.com>
2024-03-02 05:07:53 +0100

    Remove unused import and reorder URL patterns

--- img_resizer/forms.py
+++ img_resizer/forms.py
@@ -1,6 +1,3 @@
-from django import forms
-
-
 from .models import Image
 from django import forms
 
--- img_resizer/urls.py
+++ img_resizer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import HistoryView, ResizeView, DownloadView
 
 urlpatterns = [
-    path("history/", HistoryView.as_view(), name="history"),
     path("", ResizeView.as_view(), name="resize"),
+    path("history/", HistoryView.as_view(), name="history"),
     path("download/<int:pk>/", DownloadView.as_view(), name="download"),
 ]
